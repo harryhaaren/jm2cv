8b8e5e4e9723705c748801c7c2351ea469845225
Peter Nelson <peter@fuzzle.org>
2010-01-25 21:28:29 +0000

    Put common parts of cvin/cvout into cvbase class

--- cvin.h
+++ cvin.h
@@ -1,36 +1,18 @@
 #ifndef CVIN_H
 #define CVIN_H
 
-#include "client.h"
-#include "mapping.h"
+#include "cvbase.h"
 
-class CVIn : Client
+class CVIn : public CVBase
 {
 private:
-	MappingList  m_mapping_list;
 	jack_port_t *m_midi_out;
-	bool         m_ready;
-	tick_t       m_tick;
-
-	sample_t **m_buffers;
-
-	MappingList::iterator m_it_begin;
-	MappingList::iterator m_it_end;
 
 	void shutdown();
 	int process(jack_nframes_t nframes);
 
 public:
-	void add_mapping(Mapping m)
-	{
-		m_mapping_list.push_back(m);
-	}
-
 	void start();
-	void stop()
-	{
-		close();
-	}
 };
 
 #endif // CVIN_H
--- cvout.h
+++ cvout.h
@@ -1,20 +1,14 @@
-#include "client.h"
-#include "mapping.h"
+#ifndef CVOUT_H
+#define CVOUT_H
+
+#include "cvbase.h"
 
 #define MAX_MIDI_CHANNELS 16
 
-class CVOut : Client
+class CVOut : public CVBase
 {
 private:
-	MappingList  m_mapping_list;
 	jack_port_t *m_midi_in;
-	bool         m_ready;
-	tick_t       m_tick;
-
-	sample_t **m_buffers;
-
-	MappingList::iterator m_it_begin;
-	MappingList::iterator m_it_end;
 
 	char m_nrpn_msb[MAX_MIDI_CHANNELS];
 	char m_nrpn_lsb[MAX_MIDI_CHANNELS];
@@ -24,15 +18,7 @@ private:
 	int process(jack_nframes_t nframes);
 
 public:
-	void add_mapping(Mapping m)
-	{
-		m_mapping_list.push_back(m);
-	}
-
 	void start();
-	void stop()
-	{
-		close();
-	}
 };
 
+#endif // CVOUT_H
